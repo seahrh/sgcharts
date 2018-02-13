6c3322bb57ef53cd8a69e4592dc22e29f4352cc9
Ruhong <seahrh@gmail.com>
2018-02-13 21:02:46 +0800

    move to https, update deps, filter out unstable releases when checking upgrades

--- src/main/frontend/scripts/page/index.js
+++ src/main/frontend/scripts/page/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     $("#cta-coe").on("click", function() {
-        window.location.href = "http://coe.sgcharts.com/";
+        window.location.href = "https://coe.sgcharts.com/";
     });
     $("#cta-rats").on("click", function() {
-        window.location.href = "http://pay.sgcharts.com/";
+        window.location.href = "https://pay.sgcharts.com/";
     });
     $("#cta-outbreak").on("click", function() {
-        window.location.href = "http://outbreak.sgcharts.com/";
+        window.location.href = "https://outbreak.sgcharts.com/";
     });
     $("#cta-hawker").on("click", function() {
         window.location.href = "/hawker-rental";
